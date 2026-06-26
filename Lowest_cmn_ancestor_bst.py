0dd18101b0eb35e022e72a15b29b7608756ac65b
arnav-on-code <rajjarnav@gmail.com>
2026-06-26 20:51:10 +0530

    feat: method two - recursion method of lca_bst

--- Lowest_cmn_ancestor_bst.py
+++ Lowest_cmn_ancestor_bst.py
@@ -10,3 +10,13 @@ class Solution:
             else:
                 return root
             
+#recursion method!!!
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+
+        if p.val < root.val and q.val< root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        if q.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right ,p ,q)
+        return root
